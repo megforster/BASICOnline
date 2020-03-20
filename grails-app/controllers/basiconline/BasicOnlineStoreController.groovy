267f39e9fa1093f28e14f7f870d048d2a64ff7bb
meganforster <99meganforster@gmail.com>
2020-03-20 09:41:21 -0400

    Files that were left out of last push

---
+++ grails-app/controllers/basiconline/BasicOnlineStoreController.groovy
@@ -0,0 +1,135 @@
+package basiconline
+
+class BasicOnlineStoreController {
+
+    def index() { }
+
+    def renderFinalView(){
+        render(view: "storeLandingPage")
+    }
+
+    def womensSwimWear() {
+        render(view:"WomensSwimWear")
+    }
+
+    def kidsJeans() {
+        render(view:"KidsJeans")
+    }
+
+    def bath(){
+        render(view: "Bath")
+    }
+
+    def bathAccessorySet(){
+        render(view: "BathAccessorySet")
+    }
+
+    def bedding(){
+        render(view: "Bedding")
+    }
+
+    def decor(){
+        render(view: "Decour")
+    }
+
+    def graphicTeeOne(){
+        render(view: "GraphicTee1")
+    }
+
+    def graphicTeeTwo(){
+        render(view: "GraphicTee2")
+    }
+
+    def graphicTeeThree(){
+        render(view: "GraphicTee3")
+    }
+
+    def kidsCoats(){
+        render(view: "KidsCoats")
+    }
+
+    def kidsDresses(){
+        render(view: "KidsDresses")
+    }
+
+    def kidsGraphicTees(){
+        render(view: "KidsGraphicTees")
+    }
+
+    def kidsShoes(){
+        render(view: "KidsShoes")
+    }
+
+    def kidsSneaker(){
+        render(view: "KidsSneaker")
+    }
+
+    def kidsTops(){
+        render(view: "KidsTops")
+    }
+
+    def mensActiveWear(){
+        render(view: "MensActiveWear")
+    }
+
+    def mensBottoms(){
+        render(view: "MensBottoms")
+    }
+
+    def mensCoats(){
+        render(view: "MensCoats")
+    }
+
+    def mensRedCoat(){
+        render(view: "MensRedCoat")
+    }
+
+    def mensShoes(){
+        render(view: "MensShoes")
+    }
+
+    def mensTops(){
+        render(view: "MensTops")
+    }
+
+    def shoppingCart(){
+        render(view: "ShoppingCart")
+    }
+
+    def sportsBraOne(){
+        render(view: "SportsBra1")
+    }
+
+    def sportsBraTwo(){
+        render(view: "SportsBra2")
+    }
+
+    def sportsBraThree(){
+        render(view: "SportsBra3")
+    }
+
+    def womensActiveWear(){
+        render(view: "WomensActiveWear")
+    }
+
+    def womensBottoms(){
+        render(view: "WomensBottoms")
+    }
+
+    def womensCoats(){
+        render(view: "WomensCoats")
+    }
+
+    def womensLeggings(){
+        render(view: "WomensLeggings")
+    }
+
+    def womensShoes(){
+        render(view: "WomensShoes")
+    }
+
+    def womensTops(){
+        render(view: "WomensTops")
+    }
+
+}
